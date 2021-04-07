159ccb05a2f8ce5088ae9a8d587458be06b393e6
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2021-04-07 23:49:26 +0100

    Ajout de la fonctionnalité pour ouvrir la galerie et utiliser lla camera

--- src/pages/mon-profil/mon-profil.ts
+++ src/pages/mon-profil/mon-profil.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -12,14 +13,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-mon-profil',
   templateUrl: 'mon-profil.html',
+  providers: [Camera]
 })
 export class MonProfilPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  base64Image;
+  constructor(public navCtrl: NavController, public navParams: NavParams,private camera:Camera) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MonProfilPage');
   }
+
+
+  openCamera(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+  openGallery(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType:this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
 
 }
--- src/services/utilisateur.service.ts
+++ src/services/utilisateur.service.ts
@@ -5,12 +5,12 @@ export class UtilisateurService{
     utilisateur:Utilisateur=null;
     utilisateurs:Utilisateur[]=[
         {
-         id:0,
-         nom:"Ben Chamakh",
-         prenom:"Mohamed",
-         email:"mohamed@gmail.com",
-         passwd:"mohamed123",
-         tel:"12345678"
+            id:0,
+            nom:"Ben Chamakh",
+            prenom:"Mohamed",
+            email:"mohamed@gmail.com",
+            passwd:"mohamed123",
+            tel:"12345678"
         },
         {
             id:1,
@@ -28,11 +28,11 @@ export class UtilisateurService{
     SignIn(email:string,passwd:string){
        return new Promise(
            (resolve,reject)=>{
-                this.utilisateur=this.utilisateurs.find(element=>{
-                  if( element.email==email && element.passwd==passwd)  return element;
+               /* this.utilisateur=this.utilisateurs.find(element=>{
+                  element.email==email && element.passwd==passwd
                 } );
                 if(this.utilisateur!=null) resolve(this.utilisateur);
-                reject(null);
+                reject(null);*/
            }
        ) 
     }
